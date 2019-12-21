e7e8c8aee97b00187f0f51abd783136e84b6d1c8
xhlhub <xhl_email@126.com>
2019-12-21 18:32:34 +0800

    绘制三个圈完成

---
+++ es6/generrator.js
@@ -0,0 +1,16 @@
+//function* test(){
+//  yield 'hello'
+//  yield 'word'
+//  return 'end'
+//}
+//
+//const a = test()
+//console.log(a.next())
+//console.log(a.next())
+//console.log(a.next())
+//console.log(a.next())
+
+//异步
+function* test(){
+    console.log(123)
+}
